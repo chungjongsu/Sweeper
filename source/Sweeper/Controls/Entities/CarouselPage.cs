a9b8a6c14106196cfb7bd22f4e38400adf54dddc
Syri <junimiso04@naver.com>
2021-01-27 15:50:38 +0900

    Initial commit.

---
+++ source/Sweeper/Controls/Entities/CarouselPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sweeper.Controls.Entities
+{
+    internal class CarouselPage
+    {
+        #region ::Fields & Properties::
+
+        private readonly string _title = string.Empty;
+
+        internal string Title
+        {
+            get
+            {
+                return _title;
+            }
+        }
+
+        private readonly Page _content = null;
+
+        internal Page Content
+        {
+            get
+            {
+                return _content;
+            }
+        }
+
+        #endregion
+
+        #region ::Constructors::
+
+        internal CarouselPage()
+        {
+
+        }
+
+        internal CarouselPage(string title, Page content)
+        {
+            _title = title;
+            _content = content;
+        }
+
+        #endregion
+    }
+}
