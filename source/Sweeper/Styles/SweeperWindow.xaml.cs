8c9aa21a2883f29eef61e4ac1eeaccab917ff915
Syri <junimiso04@naver.com>
2021-01-29 23:41:53 +0900

    Add InLineGroupBox Fixed the border mismatching error of SweeperWindow.

--- source/Sweeper/Styles/SweeperWindow.xaml.cs
+++ source/Sweeper/Styles/SweeperWindow.xaml.cs
@@ -1,48 +1,89 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
 
 namespace Sweeper.Styles
 {
     public partial class SweeperWindow : ResourceDictionary
     {
+        #region ::Fields & Properties::
+
         Window _window;
+
+        #endregion
+
+        #region ::Constructors::
 
         public SweeperWindow()
         {
 
         }
 
+        #endregion
+
+        #region ::Methods::
+
+        private void LimitWindowSize()
+        {
+            if (GetWindow())
+            {
+                Screen currentScreen = Screen.FromPoint(Cursor.Position);
+
+                _window.MaxWidth = currentScreen.WorkingArea.Width + 16;
+                _window.MaxHeight = currentScreen.WorkingArea.Height + 16;
+            }
+        }
+
         private bool GetWindow()
         {
             if (_window == null || _window != null)
             {
-                _window = Application.Current.MainWindow;
+                _window = System.Windows.Application.Current.MainWindow;
                 return true;
             }
             else
                 return false;
         }
 
-        private void Minimize(object sender, RoutedEventArgs e)
+        #endregion
+
+        #region ::Event Subscribers::
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LimitWindowSize();
+        }
+
+        private void OnMinimize(object sender, RoutedEventArgs e)
         {
             if (GetWindow())
                 _window.WindowState = WindowState.Minimized;
         }
 
-        private void Maximize(object sender, RoutedEventArgs e)
+        private void OnMaximize(object sender, RoutedEventArgs e)
         {
             if (GetWindow())
             {
                 if (_window.WindowState == WindowState.Maximized)
+                {
                     _window.WindowState = WindowState.Normal;
+                }
                 else
+                {
+                    LimitWindowSize();
                     _window.WindowState = WindowState.Maximized;
+                }
             }
         }
 
-        private void Close(object sender, RoutedEventArgs e)
+        private void OnClose(object sender, RoutedEventArgs e)
         {
             if (GetWindow())
                 _window.Close();
         }
+
+        #endregion
     }
 }
